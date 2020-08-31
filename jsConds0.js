2813e58cf1ae4919969ca683e73bd2dd1d514d37
langehk <32093132+langehk@users.noreply.github.com>
2020-08-31 20:50:03 +0200

    calc vat & strings

--- jsConds0.js
+++ jsConds0.js
@@ -50,6 +50,35 @@ function calcVat(a) {
   vatField.innerHTML = result;
 }
 
+function first(s) {
+  s = "JuperDupe20oolStrinS";
+
+  document.writeln("output: " + s.charAt(0));
+}
+
+first();
+
+function last(s) {
+  s = "JuperDupe20oolStrinS";
+  let lastCharInString = s[s.length - 1];
+
+  document.writeln(lastCharInString);
+}
+
+last();
+
+// Skriver et bogstav ud hvis det er ulige antal, hvis ikke skriver den to tal.
+function middle(s) {
+  s = "JuperDupe20oolStrinS";
+
+  let middleChar =
+    s.length % 2 ? s.substr(s.length / 2, 1) : s.substr(s.length / 2 - 1, 2);
+
+  document.writeln(middleChar);
+}
+
+middle();
+
 /*
 flip = Math.random();
 flip = Math.floor(flip * 2 + 1);
