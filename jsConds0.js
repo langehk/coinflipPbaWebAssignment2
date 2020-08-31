363a304699a7a478e4c007a0b296d1ec06f69b69
langehk <32093132+langehk@users.noreply.github.com>
2020-08-31 20:22:40 +0200

    vat calc

---
+++ jsConds0.js
@@ -0,0 +1,106 @@
+let dice1;
+let dice2;
+let flip = 10;
+
+// Rolls two dice, from 1-6
+function rollDice() {
+  for (let i = 0; i < flip; i++) {
+    dice1 = Math.floor(Math.random() * 6) + 1;
+    dice2 = Math.floor(Math.random() * 6) + 1;
+
+    console.log("DiceRoll-" + i + ": " + dice1 + " & " + dice2);
+  }
+}
+
+rollDice();
+
+var vatField = document.getElementById("vatField");
+
+function addVat(a) {
+  vat = 1.25;
+
+  a = prompt("Enter amount");
+
+  result = a * vat;
+
+  vatField.innerHTML = result;
+}
+
+function subVat(a) {
+  vat = 0.75;
+
+  a = prompt("Enter amount");
+
+  result = a * vat;
+
+  vatField.innerHTML = result;
+}
+
+function calcVat(a) {
+  vatCalc = 1.25;
+
+  //125
+  a = prompt("Enter amount");
+
+  //100
+  temp = a / vatCalc;
+  debugger;
+  result = a - temp;
+
+  vatField.innerHTML = result;
+}
+
+/*
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1) tails++;
+else heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1) tails++;
+else heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1) tails++;
+else heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1) tails++;
+else heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1) tails++;
+else heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1) tails++;
+else heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1) tails++;
+else heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1) tails++;
+else heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1) tails++;
+else heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1) tails++;
+else heads++;
+
+console.log("heads: " + heads);
+console.log("tails: " + tails);
+*/
